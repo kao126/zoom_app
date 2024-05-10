baf2e029376705125da2839200eb60d8c000cff2
oshima <oshima@fundely.co.jp>
2024-05-10 17:59:49 +0900

    update: add form items

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -19,11 +19,12 @@ import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Progress } from '@/components/ui/progress';
 import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 export default function Form() {
   return (
-    <main>
-      <Card className="w-[700px] mx-auto">
+    <main className="h-dvh flex justify-center items-center">
+      <Card className="w-[700px] m-auto">
         <CardHeader>
           <CardTitle>Application Form</CardTitle>
           <CardDescription>Zoomウェビナー申し込みフォーム</CardDescription>
@@ -46,26 +47,44 @@ export default function Form() {
                 <Label htmlFor="email">メールアドレス</Label>
                 <Input type="email" placeholder="email" id="email" />
               </div>
-              <div className="flex flex-col space-y-1.5">
-                <Label htmlFor="age">年齢</Label>
-                <Select>
-                  <SelectTrigger id="age">
-                    <SelectValue placeholder="Select..." />
-                  </SelectTrigger>
-                  <SelectContent position="popper">
-                    <SelectItem value="10s">10-19</SelectItem>
-                    <SelectItem value="20s">20-29</SelectItem>
-                    <SelectItem value="30s">30-39</SelectItem>
-                    <SelectItem value="40s">40-49</SelectItem>
-                    <SelectItem value="50s">50-59</SelectItem>
-                    <SelectItem value="over60">60-</SelectItem>
-                  </SelectContent>
-                </Select>
+              <div className="flex space-x-1.5">
+                <div className="w-full flex flex-col space-y-1.5">
+                  <Label htmlFor="age">年齢</Label>
+                  <Select>
+                    <SelectTrigger id="age">
+                      <SelectValue placeholder="Select..." />
+                    </SelectTrigger>
+                    <SelectContent position="popper">
+                      <SelectItem value="10s">10-19</SelectItem>
+                      <SelectItem value="20s">20-29</SelectItem>
+                      <SelectItem value="30s">30-39</SelectItem>
+                      <SelectItem value="40s">40-49</SelectItem>
+                      <SelectItem value="50s">50-59</SelectItem>
+                      <SelectItem value="over60">60-</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </div>
+                <div className="w-full flex flex-col space-y-1.5">
+                  <Label htmlFor="gender">性別</Label>
+                  <Select>
+                    <SelectTrigger id="gender">
+                      <SelectValue placeholder="Select..." />
+                    </SelectTrigger>
+                    <SelectContent position="popper">
+                      <SelectItem value="male">男性</SelectItem>
+                      <SelectItem value="female">女性</SelectItem>
+                      <SelectItem value="x">未回答</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </div>
               </div>
             </div>
           </form>
         </CardContent>
-        <CardFooter className="justify-center">
+        <CardFooter className="justify-center gap-10">
+          <Button variant="outline" asChild>
+            <Link href="/">Cancel</Link>
+          </Button>
           <Button>申し込む</Button>
         </CardFooter>
       </Card>
