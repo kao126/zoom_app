debf3596d01e4a315e92ac9875b926f72c9a9d6c
oshima <oshima@fundely.co.jp>
2024-05-13 11:45:16 +0900

    feat: add participants page on admin page

---
+++ app/admin/(seminar)/[seminarId]/participants/page.tsx
@@ -0,0 +1,81 @@
+import { Payment, columns } from './columns';
+import { DataTable } from './data-table';
+
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: '1',
+      name: 'James',
+      email: 'a@example.com',
+      age: '10s',
+      gender: 'x',
+      status: 'pending',
+    },
+    {
+      id: '2',
+      name: 'Ken',
+      email: 'b@example.com',
+      age: '10s',
+      gender: 'male',
+      status: 'pending',
+    },
+    {
+      id: '3',
+      name: 'Kylie',
+      email: 'c@example.com',
+      age: '10s',
+      gender: 'female',
+      status: 'pending',
+    },
+    {
+      id: '4',
+      name: 'Chris',
+      email: 'd@example.com',
+      age: '10s',
+      gender: 'male',
+      status: 'pending',
+    },
+    {
+      id: '5',
+      name: 'Ronald',
+      email: 'e@example.com',
+      age: '10s',
+      gender: 'male',
+      status: 'pending',
+    },
+    {
+      id: '6',
+      name: 'Jessy',
+      email: 'f@example.com',
+      age: '10s',
+      gender: 'female',
+      status: 'pending',
+    },
+    {
+      id: '7',
+      name: 'Alex',
+      email: 'g@example.com',
+      age: '10s',
+      gender: 'x',
+      status: 'success',
+    },
+    // ...
+  ];
+}
+
+export default async function Participants({
+  params,
+}: {
+  params: { seminarId: number };
+}) {
+  const data = await getData();
+
+  return (
+    <main className="h-dvh flex justify-center items-center">
+      <div className="container m-auto py-10">
+        <DataTable columns={columns} data={data} />
+      </div>
+    </main>
+  );
+}
--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,89 +1,13 @@
-// import {
-//   Table,
-//   TableBody,
-//   TableCaption,
-//   TableCell,
-//   TableHead,
-//   TableHeader,
-//   TableRow,
-// } from '@/components/ui/table';
-
-import Link from 'next/link';
-import { Payment, columns } from './columns';
-import { DataTable } from './data-table';
 import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
-async function getData(): Promise<Payment[]> {
-  // Fetch data from your API here.
-  return [
-    {
-      id: '1',
-      name: 'James',
-      email: 'a@example.com',
-      age: '10s',
-      gender: 'x',
-      status: 'pending',
-    },
-    {
-      id: '2',
-      name: 'Ken',
-      email: 'b@example.com',
-      age: '10s',
-      gender: 'male',
-      status: 'pending',
-    },
-    {
-      id: '3',
-      name: 'Kylie',
-      email: 'c@example.com',
-      age: '10s',
-      gender: 'female',
-      status: 'pending',
-    },
-    {
-      id: '4',
-      name: 'Chris',
-      email: 'd@example.com',
-      age: '10s',
-      gender: 'male',
-      status: 'pending',
-    },
-    {
-      id: '5',
-      name: 'Ronald',
-      email: 'e@example.com',
-      age: '10s',
-      gender: 'male',
-      status: 'pending',
-    },
-    {
-      id: '6',
-      name: 'Jessy',
-      email: 'f@example.com',
-      age: '10s',
-      gender: 'female',
-      status: 'pending',
-    },
-    {
-      id: '7',
-      name: 'Alex',
-      email: 'g@example.com',
-      age: '10s',
-      gender: 'x',
-      status: 'success',
-    },
-    // ...
-  ];
-}
-
-export default async function Admin() {
-  const data = await getData();
-
+export default function Admin() {
   return (
     <main className="h-dvh flex justify-center items-center">
-      <div className="container m-auto py-10">
-        <DataTable columns={columns} data={data} />
-      </div>
+      <h1>adimin top</h1>
+      <Button asChild>
+        <Link href="/admin/schedule/new">create new Schedule</Link>
+      </Button>
     </main>
   );
 }
